78b0bc81719cd616ee13bd35c5d0ae496f63f5d0
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2020-01-16 12:47:39 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TheMonsterSlayer
-Created with CodeSandbox
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="title">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "Hey"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
